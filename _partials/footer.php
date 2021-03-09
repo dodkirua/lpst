fa3e9954cec55c6c4df6ea13833d893b14770803
ChloeArd <chloe.ardoise@gmail.com>
2021-03-09 16:49:56 +0100

    contenue de la page produits

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,7 +1,20 @@
-<footer>
-    <a class="network" href="#"><i class="network_i fab fa-facebook"></i></a>
-    <a class="network" href="#"><i class="network_i fas fa-envelope"></i></a>
-    <a class="network" href="#"><i class="network_i fas fa-phone-square-alt"></i></a>
+<footer class="flexColumn">
+    <div>
+        <a class="network" href="#"><i class="network_i fab fa-facebook"></i></a>
+        <a class="network" href="#"><i class="network_i fas fa-envelope"></i></a>
+        <a class="network" href="#"><i class="network_i fas fa-phone-square-alt"></i></a>
+    </div>
+
+    <div class="flexRow">
+        <a class="linkFooter" href="#">Recherche</a>
+        <a class="linkFooter" href="#">Conditions Générales de Vente</a>
+        <a class="linkFooter" href="#">Mention Légales</a>
+        <a class="linkFooter" href="#">Politiques de confidentialité</a>
+        <a class="linkFooter" href="#">Aide</a>
+        <a class="linkFooter" href="#">Contact</a>
+
+
+    </div>
 </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
