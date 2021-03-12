004767be430090bfa7d8e6a7bba66b8f995b645f
ChloeArd <chloe.ardoise@gmail.com>
2021-03-12 11:17:44 +0100

    création du profil de l'utilisateur

--- pages/account.php
+++ pages/account.php
@@ -7,12 +7,33 @@ include $_SERVER['DOCUMENT_ROOT'] . "/_partials/menu.php";
 ?>
 
 <main class="flexRow">
-    <h1">Bonjour, <span class="colorGreen">.....</span></h1>
-    <section class="innformation">
+    <h1 id="hello">Bonjour, <span class="colorGreen">.....</span></h1>
+    <section id="informationProfil">
+        <div id="profil" class="flexColumn flexCenter">
+            <img id="photoProfil" src="http://www.clker.com/cliparts/d/L/P/X/z/i/no-image-icon-md.png" alt="photoProfil">
+            <p class="nameStaff colorBlue">Prénom NOM</p>
+        </div>
+        <div class="separatorHorizontal"></div>
+        <div class="select">
+            <p class="colorWhite">Mes informations</p>
+        </div>
+        <div class="separatorHorizontal"></div>
+        <div class="select">
+            <p class="colorWhite">Mes commandes</p>
+        </div>
+        <div class="separatorHorizontal"></div>
+        <div class="select">
+            <p class="colorWhite">Mes paniers sauvegardés</p>
+        </div>
+        <div class="separatorHorizontal"></div>
+        <div class="select">
+            <p class="colorWhite">Mes paramètres</p>
+        </div>
 
     </section>
-    <section>
 
+    <section id="otherInformation">
+        <p> Mes informations ...</p>
     </section>
 </main>
 
