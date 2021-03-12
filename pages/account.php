f8ff89edbf4c666c78119af06f55ce1b5bfe0af3
ChloeArd <chloe.ardoise@gmail.com>
2021-03-12 12:00:16 +0100

    création du profil de l'utilisateur

--- pages/account.php
+++ pages/account.php
@@ -10,7 +10,7 @@ include $_SERVER['DOCUMENT_ROOT'] . "/_partials/menu.php";
     <h1 id="hello">Bonjour, <span class="colorGreen">.....</span></h1>
     <section id="informationProfil">
         <div id="profil" class="flexColumn flexCenter">
-            <img id="photoProfil" src="http://www.clker.com/cliparts/d/L/P/X/z/i/no-image-icon-md.png" alt="photoProfil">
+            <img class="photoProfil" src="http://www.clker.com/cliparts/d/L/P/X/z/i/no-image-icon-md.png" alt="photoProfil">
             <p class="nameStaff colorBlue">Prénom NOM</p>
         </div>
         <div class="separatorHorizontal"></div>
@@ -33,7 +33,42 @@ include $_SERVER['DOCUMENT_ROOT'] . "/_partials/menu.php";
     </section>
 
     <section id="otherInformation">
-        <p> Mes informations ...</p>
+        <div id="informationAccount">
+            <h2 class="subtitle"> Mes coordonées</h2>
+            <div class="flexColumn flexCenter">
+                <img class="photoProfil" src="http://www.clker.com/cliparts/d/L/P/X/z/i/no-image-icon-md.png" alt="photoProfil">
+                <p class="colorBlue">Prénom </p>
+                <p class="whiteBorder">(Prénom)</p>
+                <p class="colorBlue">Nom </p>
+                <p class="whiteBorder">(Nom)</p>
+                <p class="colorBlue">Email </p>
+                <p class="whiteBorder">(prenom.nom@mail.com)</p>
+                <div class="flexCenter">
+                    <button id="modifyPorfil" class="send modify">Modifier le profil</button>
+                </div>
+            </div>
+            <div class="separatorHorizontal"></div>
+            <h2 class="subtitle"> Mes adresses</h2>
+            <h3 class="colorGrey "> Adresse de facturation</h3>
+            <h3 class="colorGrey"> Adresse de livraison</h3>
+
+            <div class="separatorHorizontal"></div>
+            <div class="flexColumn">
+                <h2 class="subtitle"> Changer mon mot de passe</h2>
+                <label class="colorBlue" for="currentPwd">Mon mot de passe actuel</label>
+                <input id="currentPwd" name="currentPwd" type="password">
+                <label class="colorBlue" for="newPwd">Mon nouveau mot de passe</label>
+                <input name="newPwd" id="newPwd" type="password">
+                <div class="flexCenter">
+                    <button id="modifypwd" class="send modify">Valider</button>
+                </div>
+            </div>
+
+            <div class="flexCenter">
+                <button class="redButton">Déconnexion</button>
+            </div>
+        </div>
+
     </section>
 </main>
 
