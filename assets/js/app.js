2a7d007ba3b15f38461defd3c4a8607d05251a6c
dodkirua <pybouttefeux@gmail.com>
2021-03-22 14:37:15 +0100

    mod User

--- assets/js/app.js
+++ assets/js/app.js
@@ -224,20 +224,21 @@ $("#billingAddress").click(function () {
 
 //Condition for the total of the articles, if the total is below 50 then there is no delivery,
 //if it is between 50 and 100 then it is 5 € and if it is equal to or greater than 100 € then it is free.
-let totalBuy = document.getElementById("totalBuy").innerHTML = 145;
-if (totalBuy < 50) {
-    document.getElementById("delivery2").style.display = "none";
-}
-if (50 <= totalBuy < 100) {
-    document.getElementById("buyDelivery").innerHTML = "A partir de " +  5 + "€";
-    document.getElementById("buyDelivery2").innerHTML = 5 + "€";
+if (document.getElementById("totalBuy")){
+    let totalBuy = document.getElementById("totalBuy").innerHTML = 145;
+    if (totalBuy < 50) {
+        document.getElementById("delivery2").style.display = "none";
+    }
+    if (50 <= totalBuy < 100) {
+        document.getElementById("buyDelivery").innerHTML = "A partir de " +  5 + "€";
+        document.getElementById("buyDelivery2").innerHTML = 5 + "€";
 
+    }
+    if (totalBuy >= 100) {
+        document.getElementById("buyDelivery").innerHTML = "<strong>GRATUIT </strong>";
+        document.getElementById("buyDelivery2").innerHTML = "<strong>GRATUIT </strong>";
+    }
 }
-if (totalBuy >= 100) {
-    document.getElementById("buyDelivery").innerHTML = "<strong>GRATUIT </strong>";
-    document.getElementById("buyDelivery2").innerHTML = "<strong>GRATUIT </strong>";
-}
-
 
 
 if (document.getElementById("complements")) {
