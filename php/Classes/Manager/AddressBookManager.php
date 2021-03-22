0ff3b93db56491f6d7ee24a094ebc646a4be0779
dodkirua <pybouttefeux@gmail.com>
2021-03-22 14:15:12 +0100

    mod import

--- php/Classes/Manager/AddressBookManager.php
+++ php/Classes/Manager/AddressBookManager.php
@@ -1,6 +1,5 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . "/php/Classes/BD.php";
-require $_SERVER['DOCUMENT_ROOT'] . "/php/Classes/User.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/php/import/importManager.php";
 
 
 class AddressBookManager{
