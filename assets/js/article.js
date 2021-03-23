afcf48dfb1f1f255bcc3732ec6a48cfd5e18a0f5
dodkirua <pybouttefeux@gmail.com>
2021-03-23 14:37:40 +0100

    mod $_session dans menu

--- assets/js/article.js
+++ assets/js/article.js
@@ -11,7 +11,9 @@ export function itemQuantity (priceArticle, idQuantity, idPrice, total, idMore, 
     for (let i = 0; i < 2; i++) {
         let price = priceArticle;
         let addArticle = priceArticle;
-        let numberArticle1 = document.getElementsByClassName(idQuantity)[i].innerHTML;
+        let idQuant = document.getElementsByClassName(idQuantity);
+        console.log(idQuant);
+        let numberArticle1 = idQuant[i].innerHTML;
         let valueArticle1 = Number.parseInt(numberArticle1);
         let totalPrice = document.getElementsByClassName(total)[i];
         totalPrice.innerHTML = "<strong>" + price + "€ </strong>";
@@ -19,17 +21,17 @@ export function itemQuantity (priceArticle, idQuantity, idPrice, total, idMore, 
 
         document.getElementsByClassName(idMore)[i].addEventListener("click", function () {
             totalPrice.innerHTML = "<strong>" + (price += addArticle).toFixed(2) + "€ </strong>";
-            document.getElementsByClassName(idQuantity)[i].innerHTML = valueArticle1 += 1;
+            numberArticle1 = valueArticle1 += 1;
         });
 
         document.getElementsByClassName(idLess)[i].addEventListener("click", function () {
-            if (document.getElementsByClassName(idQuantity)[i].innerHTML === "1" || document.getElementsByClassName(idQuantity)[i].innerHTML <= "1") {
+            if (numberArticle1 === "1" || numberArticle1 <= "1") {
                 totalPrice.innerHTML = "<strong>" + price.toFixed(2) + "€ </strong>";
-                document.getElementsByClassName(idQuantity)[i].innerHTML = 1;
+                document.getElementsByClassName(idQuantity)[i].innerHTML = "1";
             }
             else {
-                document.getElementsByClassName(total)[i].innerHTML = "<strong>" + (price -= addArticle).toFixed(2) + "€ </strong>";
-                document.getElementsByClassName(idQuantity)[i].innerHTML = valueArticle1 -= 1;
+                totalPrice.innerHTML = "<strong>" + (price -= addArticle).toFixed(2) + "€ </strong>";
+                numberArticle1.innerHTML = valueArticle1 -= 1;
             }
         });
     }
