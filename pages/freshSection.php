5078606b0e3d91134598f8314d6df0688fb55c03
ChloeArd <chloe.ardoise@gmail.com>
2021-04-01 16:30:49 +0200

    Ajout d'input dans les formulaires, pour ajouter plusieurs choses

--- pages/freshSection.php
+++ pages/freshSection.php
@@ -2,11 +2,44 @@
 session_start();
 $title = "LPST : Rayon frais";
 
+require_once $_SERVER['DOCUMENT_ROOT'] . "/php/Classes/Manager/InformationManager.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/_partials/header.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/_partials/menu.php";
+
+
+$manager = new InformationManager();
+$informations = $manager->getInformationByLocation("freshSection.php");
 ?>
     <main>
-        <h1 class="colorBlue">RAYON <span class="colorGreen"> FRAIS</span></h1>
+        <h1 class="colorBlue">RAYON<span class="colorGreen"> FRAIS</span></h1>
+        <?php
+        $token = true;
+        foreach ($informations as $item) {
+            $information = $item->getData();
+            if ($token) {
+                containerShop1($information["image"], $information["title"], $information["description"]);
+                if (isset($_SESSION["user"]["id"])) {
+                    if ($_SESSION['user']['role'] != 2) {
+                        echo "<button class='buttonClassic'><i class='far fa-trash-alt'></i></button>
+                                <button class='buttonClassic'><i class='fas fa-edit'></i></button>";
+                    }
+                }
+                else {
+                    echo "";
+                }
+            }
+            else {
+                containerShop2($information["image"], $information["title"], $information["description"]);
+                if (isset($_SESSION["user"]["id"])) {
+                    if ($_SESSION['user']['role'] !== 2) {
+                        echo "<button class='buttonClassic'><i class='far fa-trash-alt'></i></button>
+                                <button class='buttonClassic'><i class='fas fa-edit'></i></button>";
+                    }
+                }
+            }
+            $token = !$token;
+        }
+        ?>
     </main>
 
 <?php
