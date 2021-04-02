e88660eff91962bf1fe2d3b868b11927633a39e9
dodkirua <pybouttefeux@gmail.com>
2021-04-02 10:04:28 +0200

    modify password UserManager.php

--- php/modifyPassword.php
+++ php/modifyPassword.php
@@ -3,12 +3,29 @@ session_start();
 require $_SERVER['DOCUMENT_ROOT'] . "/php/Classes/Manager/UserManager.php";
 require $_SERVER['DOCUMENT_ROOT'] . "/php/function.php";
 
-if (isset($_POST["currentPwd"], $_POST["newPwd"])) {
+if (isset($_POST["currentPwd"]) && isset($_POST["password"]) && isset($_POST["repeatPassword"])) {
     $currentPass = sanitize($_REQUEST['currentPwd']);
-    $newPass = sanitize($_REQUEST['newPwd']);
+    $newPass = sanitize($_REQUEST['password']);
+    $verifPass = sanitize($_POST["repeatPassword"]);
 
     if (password_verify($currentPass, $_SESSION["user"]["pass"])) {
         echo "ils correspondent";
-        //modifier le mdp dans la BDD par le nouveau mdp entrer.
+        if ($newPass === $verifPass){
+            if (checkPass($newPass)){
+
+            }
+            else {
+                header('Location: ../../pages/account.php?e=3');
+            }
+        }
+        else {
+            header('Location: ../../pages/account.php?e=2');
+        }
     }
+    else{
+        header('Location: ../../pages/account.php?e=1');
+    }
+}
+else {
+    header('Location: ../../pages/account.php?e=0');
 }
